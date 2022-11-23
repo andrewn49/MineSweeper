d5c3bb790ac926e0960c0a4ab39ac47d3e24310e
Andrew Neely <andrewneely36@gmail.com>
2022-11-23 16:20:48 +0000

    Adding testing things again

--- src/test/java/TileTest.java
+++ src/test/java/TileTest.java
@@ -5,7 +5,20 @@ import org.junit.jupiter.api.Test;
 public class TileTest {
     @Test
     public void testConstructor(){
-        Tile tile = new Tile(false);
-        Assertions.assertEquals(false, tile.getIsbomb());
+        Tile tile1 = new Tile(false);
+        Assertions.assertFalse(tile1.getIsbomb());
+        Tile tile2 = new Tile(true);
+        Assertions.assertTrue(tile2.getIsbomb());
+    }
+
+    @Test
+    public void testTilestates(){
+        Tile tile3 = new Tile(false);
+        Assertions.assertEquals(0, tile3.getTilestate(), "Tilestate is wrong: " + tile3.getTilestate());
+        tile3.flagMe();
+        Assertions.assertEquals(4, tile3.getTilestate(), "Tilestate is wrong: " + tile3.getTilestate());
+        tile3.flagMe();
+        tile3.revealMe();
+        Assertions.assertEquals(1, tile3.getTilestate(), "Tilestate is wrong: " + tile3.getTilestate());
     }
 }
